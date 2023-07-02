8c8f7f2eefe1296f476daf76ba73e7988d305bb2
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-07-02 05:53:26 +0500

    creating smart contract

--- blockchain/contracts/CrowdFunding.sol
+++ blockchain/contracts/CrowdFunding.sol
@@ -2,5 +2,57 @@
 pragma solidity ^0.8.9;
 
 contract CrowdFunding {
-    constructor() {}
+    constructor() {
+        
+    }
+
+    struct Campaign {
+        address  owner ;
+        string title ;
+        string description ;
+        uint256 target;
+        uint256 deadline;
+        string image;
+        uint256 amountCollected;
+        address[] donators;
+        uint256[] donations;
+    }
+
+    mapping(uint256 => Campaign) public campagins;
+
+    uint256 public numberOFCampaigns = 0;
+
+
+    function createCampaign (address _owner , string memory _title  , string memory _description , uint256 _target , uint256 _deadline , string memory _image )  public returns(uint256) {
+        Campaign storage campaign = campagins[numberOFCampaigns] ;
+
+        require(campaign.deadline <  block.timestamp , "Deadline must be Future Date" );
+
+        campaign.owner = _owner;
+        campaign.deadline = _deadline ;
+        campaign.title = _title;
+        campaign.description  =  _description;
+        campaign.target = _target;
+        campaign.image = _image;
+        campaign.amountCollected = 0 ;
+
+        numberOFCampaigns = numberOFCampaigns + 1 ;
+
+        return numberOFCampaigns - 1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable{
+        uint256 amount  =  msg.value;
+
+        Campaign storage campaign = campagins[_id];
+        campaign.donators.push(msg.sender)
+        campaign.donators.push(amount)
+
+        (bool sent) = payable(campaign.owner).call{value : amount}("");
+
+        if(sent , ){
+            campagin.amountCollected = campagin.amountCollected +  amount;
+        }
+    }
+
 }
