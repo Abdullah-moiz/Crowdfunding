b0f9066233525b4d76acec30deb4b320874964e7
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-07-03 07:19:44 +0500

    Improving Ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { Route, Routes } from 'react-router-dom'
 import Landing from "./pages/Landing";
+import CreateCampaign from "./pages/CreateCampaign";
 
 
 export default function Home() {
   return (
     <Routes>
       <Route element={<Landing />} path="/"  />
+      <Route element={<CreateCampaign />} path="/create-campaign"  />
     </Routes>
   );
 }
---
+++ frontend/src/pages/CreateCampaign.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function CreateCampaign() {
+    return (
+        <div className='w-full h-screen bg-slate-950'>
+            <div className="text-sm breadcrumbs text-white">
+                <ul>
+                    <li>
+                        <Link to="/">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path></svg>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="w-4 h-4 mr-2 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 13h6m-3-3v6m5 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+                        Create Campaign
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
