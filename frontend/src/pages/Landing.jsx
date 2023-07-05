e4a9c8baa2760f2476464ad88be52c9e212071ca
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-07-05 08:44:12 +0500

    getting all Crowdfunding events

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar, Footer, Search, Campaign } from '../components';
+import { useContract, useContractRead } from '@thirdweb-dev/react';
 
 
 export default function Landing() {
+
+    const { contract } = useContract("0x853C53E1BaAe5bb341fC0Ac7Ff67B932a7F6fe58");
+
+    const { data, isLoading, error } = useContractRead(contract, "getCampaign");
+
+
+
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+
     return (
         <div className='w-full flex flex-col h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black '>
             <Navbar />
