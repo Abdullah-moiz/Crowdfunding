839c5210615a622f6489ba2fe1b678f7e38228d7
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-07-10 06:41:50 +0500

    fixing bugs

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -29,8 +29,8 @@ export default function Search() {
             const newData = filteredData?.filter((item) => {
                 return item?.title?.toLowerCase().includes(searchText?.toLowerCase())
             })
-            if (newData.length === 0) return toast.error('No Campaign Found');
-            newData?.length > 0 ? dispatch(setFilteredCampaigns(newData)) : dispatch(setFilteredCampaigns(null))
+            if (searchText !== "" && newData.length === 0) return toast.error('No Campaign Found');
+            newData?.length > 0 && dispatch(setFilteredCampaigns(newData))
         }, 500);
 
         return () => clearTimeout(delaySearch);
@@ -43,7 +43,7 @@ export default function Search() {
         <div className={`${search ? "flex" : "hidden"} relative w-full my-2  flex-col h-24  px-4  items-center justify-center `}>
             <input onChange={(e) => setSearchText(e.target.value)} type="search" id='search' placeholder="Search Campagins..." className="input input-bordered input-secondary w-full max-w-xs" />
             <button onClick={HandleSearch} className='btn btn-circle  absolute top-2 btn-neutral text-red-600 right-5 md:right-20 '><ImCross /></button>
-            
+
         </div>
     )
 }
