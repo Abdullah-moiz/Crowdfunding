57206a5ec473fd7ac408e9600c2d700c248054f8
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-07-10 06:37:34 +0500

    fixing bugs

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,32 +2,39 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ImCross } from 'react-icons/im'
 import { setOpenSearch } from '../slices/NavSlices';
-import {  setFilteredCampaigns } from '../slices/smartContractSlice';
+import { setFilteredCampaigns } from '../slices/smartContractSlice';
+import { toast } from 'react-toastify';
 
 export default function Search() {
     const dispatch = useDispatch();
     const [searchText, setSearchText] = useState("");
-    const [filteredData , setFilteredData ]  =  useState([]);
+    const [filteredData, setFilteredData] = useState([]);
     const campaignData = useSelector(state => state.Contract.contractData)
     const data = campaignData?.slice(6)
     const search = useSelector((state) => state.Navigation.Opensearch)
 
-    useEffect(() =>{
+    useEffect(() => {
         setFilteredData(data)
-    },[campaignData])
+    }, [campaignData])
 
     const HandleSearch = () => {
         dispatch(setOpenSearch(false))
     }
 
 
-    useEffect(() => {
-        const newData = filteredData?.filter((item) => {
-            return item?.title?.toLowerCase().includes(searchText?.toLowerCase())
-        })
-        newData?.length > 0 ? dispatch(setFilteredCampaigns(newData)) : dispatch(setFilteredCampaigns(null))
-    }, [searchText ])
 
+
+    useEffect(() => {
+        const delaySearch = setTimeout(() => {
+            const newData = filteredData?.filter((item) => {
+                return item?.title?.toLowerCase().includes(searchText?.toLowerCase())
+            })
+            if (newData.length === 0) return toast.error('No Campaign Found');
+            newData?.length > 0 ? dispatch(setFilteredCampaigns(newData)) : dispatch(setFilteredCampaigns(null))
+        }, 500);
+
+        return () => clearTimeout(delaySearch);
+    }, [searchText]);
 
 
 
@@ -36,6 +43,7 @@ export default function Search() {
         <div className={`${search ? "flex" : "hidden"} relative w-full my-2  flex-col h-24  px-4  items-center justify-center `}>
             <input onChange={(e) => setSearchText(e.target.value)} type="search" id='search' placeholder="Search Campagins..." className="input input-bordered input-secondary w-full max-w-xs" />
             <button onClick={HandleSearch} className='btn btn-circle  absolute top-2 btn-neutral text-red-600 right-5 md:right-20 '><ImCross /></button>
+            
         </div>
     )
 }
